94c6d0028de9ca37e0ebaa509b6fe76cc0b70d26
valerian963 <valeriasantos963@gmail.com>
2022-10-13 21:46:16 -0400

    Style Classes

--- source/TableStyle.java
+++ source/TableStyle.java
@@ -1,28 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package com.mycompany.telas;
 
 import java.awt.Color;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.Component;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
         
 public class TableStyle extends JTable{
+    private  HeaderStyle header;
+    //private RowStyle cell;
     
     public TableStyle(){
+        header = new HeaderStyle();
+        //cell = new RowStyle();
         getTableHeader().setDefaultRenderer(new HeaderStyle());
+        //setDefaultRenderer(Object.class, new RowStyle());
+        //setRowHeight(30);
     }
     
+     public void setColumnAlignment(int column, int align) {
+        header.setAlignment(column, align);
+     }   
+    public void newScrollBar(JScrollPane scroll) {
+        scroll.setVerticalScrollBar(new ScrollBarCustom());
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(0,196,204));
+        scroll.setCorner(JScrollPane.UPPER_RIGHT_CORNER, panel);
+        scroll.getViewport().setBackground(new Color(255, 255, 255));
+        
+    }
     private class HeaderStyle extends DefaultTableCellRenderer{
+        private Map <Integer,Integer> alinhamento = new HashMap<>();
+        
+        public void setAlignment(int column, int align) {
+            alinhamento.put(column, align);
+        }
         
         @Override
-        public Component getTableCellRendererComponent(JTable jtable, Object o, boolean bln, boolean bln1, int i, int i1){
-            Component com = super.getTableCellRendererComponent(jtable, o, bln1, bln1, i, i1);
+        public Component getTableCellRendererComponent(JTable jtable, Object o, boolean bln, boolean bln1, int row, int column){
+            Component com = super.getTableCellRendererComponent(jtable, o, bln1, bln1, row, column);
             com.setBackground(new Color(0,196,204));
             com.setForeground(new Color(255,255,255));
             com.setFont(new java.awt.Font("Tw Cen MT Condensed", 0, 20));
+            if (alinhamento.containsKey(column)) {
+                setHorizontalAlignment(alinhamento.get(column));
+            } else {
+                setHorizontalAlignment(JLabel.CENTER);
+            }
             return com;
         }
     }
+    
+    
 }
